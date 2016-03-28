06388824edf0b4fa5073c457c89cd8805204db37
Nicole Chiang <nic.chiang@gmail.com>
2016-03-28 09:50:56 -0400

    add options and add questions are dynamic

--- test_stuff/createquiz.js
+++ test_stuff/createquiz.js
@@ -1,6 +1,14 @@
-window.onload = function() {
+// window.onload = function() {
+  $( document ).ready(function() {
 
-  $optionClass = $(".q1opt");
+
+  maxOptions = 8;
+
+  $optionClass = $("#q1opt");
+
+  $questionDiv = $("#q1");
+
+  console.log($questionDiv);
 
   console.log($optionClass);
 
@@ -12,34 +20,208 @@ window.onload = function() {
 
   // console.log($inputName)
 
+  // $lastInput = $(".last");
+  // var className = $lastInput.attr("class");
+  // console.log(className);
+  // var questionNumber = parseInt(className.replace(/[^0-9]/g, ""));
+  // console.log(questionNumber)
+  // console.log($lastInput)
+
+
+  // // add another input to option
+  // $("#add-input").click(function(event) {
+
+  //   $lastInput = $(".last");
+  //   var className = $lastInput.attr("class");
+  //   console.log(className);
+  //   var questionNumber = parseInt(className.replace(/[^0-9]/g, ""));
+  //   console.log(questionNumber)
+  //   console.log($lastInput)
+
+  //   $lastInputOption = $("#q" + questionNumber + "opt input").last();
+
+  //   // $lastInputOption = $("#q1opt input").last();
+
+  //   // console.log($lastInputOption);
+
+  //   $inputName = $lastInputOption.attr("name");
+
+  //   optionNumber = $inputName.replace(/[^0-9]/g, "");
+
+  //   console.log(optionNumber);
+
+  //   // console.log(next)
+
+  //   var add = parseInt(optionNumber) + 1;
+
+  //   console.log(add);
+
+  //   // if options equals or exceeds maximum, hide the add button
+  //   if (add >= maxOptions) {
+
+  //     $(this).hide();
+
+  //   }; // closes if statement
+
+  //   var newInput = $("<input type='text' name='opt" + add + "' class='q1'> <br>");
+
+  //   // append to div option class
+  //   // $(newInput).appendTo(".q1opt");
+  //   $(newInput).appendTo($optionClass);
 
 
 
-  $("#add-new").click(function(event) {
+  // }); // closes add-input click function
 
-    $lastInputOption = $(".q1opt input").last();
 
-    // console.log($lastInputOption);
+  // add another question
+  $(document).on("click", "#add-question", function(){ 
+  // $("#add-question").click(function(event) {
+
+    // grab last question div
+    var $last = $(".last");
+
+    // access class of the last question
+    lastClass = $last.attr("class").replace(/[^0-9]/g, "");
+
+    // next question
+    nextClassNumber = parseInt(lastClass) + 1;
+
+    console.log(lastClass)
+
+    console.log(nextClassNumber)
+
+    // // remove last class from previous question
+    // $last.removeClass("last");
+    // console.log($last);
+
+    // create div for next question
+    var $nextQuestion = $("<div></div>");
+
+    // set id and class
+    $nextQuestion.attr({
+      id: "q" + nextClassNumber,
+      class: "q" + nextClassNumber
+    });
+    console.log($nextQuestion); // confirms div created correctly
+
+    // append div to page
+    $nextQuestion.insertAfter($last);
+
+    // remove last class from previous last question
+    $last.removeClass("last");
+
+    // add last class to current question
+    $nextQuestion.addClass("last");
+
+    // remove add new option button
+    // $("#add-input").hide();
+
+    // create input for question
+    $questionText = $("<input type='text'>");
+
+    $nextQuestion.append($questionText);
+
+    // add div for the options
+    var $optionsDiv = $("<div></div>");
+
+    // set id and class
+    $optionsDiv.attr({
+      id: "q" + nextClassNumber + "opt",
+      class: "q" + nextClassNumber
+    });
+    console.log($optionsDiv); // confirms div created correctly
+
+    // append div to page
+    $optionsDiv.insertAfter($questionText);
+
+    $newInput = $("<input type='text'>");
+
+    $newInput.attr({
+      name: "opt1",
+      class: "q" + nextClassNumber
+    })
+
+    $newInput.appendTo($optionsDiv);
+
+    // add new option button
+    $insertOptionButton = $("<i>New Option</i>");
+
+    $insertOptionButton.attr({
+      id: "add-input",
+      class: "q" + nextClassNumber
+    })
+
+    $insertOptionButton.insertAfter($optionsDiv);
+
+
+  }); // closes add-question click function
+
+  // $(document).on('click', '#add-input', function(){ 
+  //    console.log("clicked document")
+  // });
+
+  // add another input to option
+  $(document).on("click", "#add-input", function(){ 
+  // $("#add-input").click(function(event) {
+
+    $lastInput = $(".last");
+    var className = $lastInput.attr("class");
+    console.log(className);
+    var questionNumber = parseInt(className.replace(/[^0-9]/g, ""));
+    console.log(questionNumber);
+
+    $optionClass = $("#q" + questionNumber + "opt");
+
+    $lastInputOption = $("#q" + questionNumber + "opt input").last();
+
+    // // $lastInputOption = $("#q1opt input").last();
+
+    console.log($lastInputOption);
 
     $inputName = $lastInputOption.attr("name");
 
     optionNumber = $inputName.replace(/[^0-9]/g, "");
 
-    console.log(optionNumber)
+    console.log(optionNumber);
 
     // console.log(next)
 
-    var add = parseInt(optionNumber) + 1
+    var add = parseInt(optionNumber) + 1;
 
-    console.log(add)
+    console.log(add);
 
-    var newInput = $("<input type='text' name='opt" + add + "' class='q1'> <br>");
+    // if options equals or exceeds maximum, hide the add button
+    if (add >= maxOptions) {
+
+      $(this).hide();
+
+    }; // closes if statement
+
+    // var newInput = $("<input type='text' name='opt" + add + "' class='q1'> <br>");
+
+    var newInput = $("<input type='text'>");
+
+    newInput.attr({
+      name: "opt" + add,
+      class: "q" + questionNumber
+    });
 
     // append to div option class
     // $(newInput).appendTo(".q1opt");
     $(newInput).appendTo($optionClass);
 
-  }); // closes click function
+
+
+  }); // closes add-input click function
+
+  
+
+
+
+
+
+
 
 
 
@@ -95,4 +277,4 @@ window.onload = function() {
   // });
 
 
-};
+}); // closes window onload
