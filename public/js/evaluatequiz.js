49b7ede135d2b5f847ec447985d3c47e33dbb84d
Nicole Chiang <nic.chiang@gmail.com>
2016-04-02 18:13:13 -0400

    testing share_open_graph for heroku

--- public/js/evaluatequiz.js
+++ public/js/evaluatequiz.js
@@ -462,6 +462,14 @@ $(document).ready(function() {
     console.log(quizData);
     console.log(userResult)
 
+    FB.ui({
+      method: 'share_open_graph',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+        object:'https://developers.facebook.com/docs/',
+      })
+      }, function(response){});
+
       // FB.ui({
       //   method: "feed",
       //   display: "popup",
