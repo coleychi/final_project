dd0f4a9d096c2848092d77b438eaf7d0531fa212
Nicole Chiang <nic.chiang@gmail.com>
2016-04-03 09:43:24 -0400

    quiz images can be uploaded

--- public/js/newquizform.js
+++ public/js/newquizform.js
@@ -13,6 +13,82 @@ $(document).ready(function() {
 
   valid = false;
 
+
+  // AWS
+  // listens for change in input
+  (function() {
+      // fileInput = document.getElementById("file-input");
+      // console.log("file input:", fileInput)
+      document.getElementById("file-input").onchange = function(){
+        console.log("user inputted a file")
+          var files = document.getElementById("file-input").files;
+          var file = files[0];
+          if(file == null){
+              alert("No file selected.");
+          }
+          else{
+            // display file name
+            $("#img-path").val(file.name)
+            get_signed_request(file);
+          }
+      };
+  })();
+
+
+  // accepts file object and retrieves signed request
+  function get_signed_request(file){
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", "/quizzes/sign_s3?file_name="+file.name+"&file_type="+file.type);
+      xhr.onreadystatechange = function(){
+          if(xhr.readyState === 4){
+              if(xhr.status === 200){
+                  var response = JSON.parse(xhr.responseText);
+                  upload_file(file, response.signed_request, response.url);
+              }
+              else{
+                  alert("Could not get signed URL.");
+              }
+          }
+      };
+      xhr.send();
+  }
+
+
+  // accepts file to be uploaded
+  function upload_file(file, signed_request, url){
+      var xhr = new XMLHttpRequest();
+      xhr.open("PUT", signed_request);
+      xhr.setRequestHeader('x-amz-acl', 'public-read');
+      xhr.onload = function() {
+          if (xhr.status === 200) {
+              // document.getElementById("preview").src = url;
+              // console.log(url)
+              document.getElementById("imgUrl").value = url;
+          }
+      };
+      xhr.onerror = function() {
+          alert("Could not upload file.");
+      };
+      xhr.send(file);
+  };
+
+  // switch to url input
+  $(document).on("click", "#switch", function(event) {
+    console.log("switch");
+    // hide image upload stuff
+    $(".image-upload").hide();
+
+    // create regulare image input
+    $imgUrlInput = $("<input>").attr({
+      type: "text",
+      name: "imgUrl",
+      placeholder: "image url"
+    }).insertAfter($("#imgUrl-label"))
+
+
+  });
+
+
   // grab hint text and save
   var hintText = $(".hint").text();
   console.log(hintText);
@@ -490,17 +566,17 @@ $(document).ready(function() {
 
 
       // run validation on all input forms with change
-      validateInput(allInputs, function() {
+      // validateInput(allInputs, function() {
 
-        console.log("is form valid?: ", valid);
+      //   console.log("is form valid?: ", valid);
 
-        if (!valid) {
-          console.log("not valid, do not submit");
-          event.preventDefault();
-        };
+      //   if (!valid) {
+      //     console.log("not valid, do not submit");
+      //     event.preventDefault();
+      //   };
 
 
-      });
+      // }); // closes validateInput(allInputs)
 
     });
 
